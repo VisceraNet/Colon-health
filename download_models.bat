581e3e2181fb512e53225d6b9910a4043cb8205d
paulsamuel-w-e <work.paulsamuel@gmail.com>
2026-01-24 14:18:51 +0530

    Reset phase2 branch to empty

--- download_models.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Wrapper to run PowerShell script
-
-REM Ensure PowerShell execution policy allows script execution
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0\download_models.ps1"
-pause
